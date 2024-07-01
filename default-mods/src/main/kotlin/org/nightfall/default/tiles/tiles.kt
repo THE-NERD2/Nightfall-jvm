4d115115fc0915d465429fff9fb9b11abe7d5dca
payton <pdp9729@gmail.com>
2024-07-01 14:28:56 -0600

    No more texturing models at runtime, fixes rendering

--- default-mods/src/main/kotlin/org/nightfall/default/tiles/tiles.kt
+++ default-mods/src/main/kotlin/org/nightfall/default/tiles/tiles.kt
@@ -2,4 +2,4 @@ package org.nightfall.default.tiles
 
 import org.nightfall.materials.Tile
 
-object GrassTile: Tile("default-mods/src/main/resources/models/cube_1111.gltf", "default-mods/src/main/resources/textures/grass.png")
+object GrassTile: Tile("default-mods/src/main/resources/models/grass_1111.gltf")
--- framework/src/main/kotlin/org/nightfall/materials/materials.kt
+++ framework/src/main/kotlin/org/nightfall/materials/materials.kt
@@ -1,20 +1,16 @@
 package org.nightfall.materials
 
-import com.badlogic.gdx.Gdx
 import com.badlogic.gdx.assets.AssetManager
 import com.badlogic.gdx.assets.loaders.FileHandleResolver
-import com.badlogic.gdx.graphics.Texture
-import com.badlogic.gdx.graphics.g3d.Material
 import com.badlogic.gdx.graphics.g3d.Model
 import com.badlogic.gdx.graphics.g3d.ModelInstance
-import com.badlogic.gdx.graphics.g3d.attributes.TextureAttribute
 import com.badlogic.gdx.math.Vector3
 import net.mgsx.gltf.loaders.gltf.GLTFAssetLoader
 import net.mgsx.gltf.scene3d.scene.SceneAsset
 import org.nightfall.worldgen.World
 import java.io.Serializable
 
-open class Tile(val modelPath: String, val texturePath: String, resolver: FileHandleResolver? = null) {
+open class Tile(val modelPath: String, resolver: FileHandleResolver? = null) {
     private val mgr = AssetManager(resolver)
     lateinit var model: Model
 
@@ -27,14 +23,6 @@ open class Tile(val modelPath: String, val texturePath: String, resolver: FileHa
     }
     internal fun initialize() {
         model = mgr.get(modelPath, SceneAsset::class.java).scene.model
-        val texture = Texture(Gdx.files.internal(texturePath))
-        val material = Material(TextureAttribute.createDiffuse(texture))
-        /*for(node in model.nodes) {
-            for(part in node.parts) {
-                part.material = material
-            }
-        }*/
-        model.materials[0] = material
     }
     open fun update(instance: TileInstance<*>, dt: Double) {}
 }
